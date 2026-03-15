9f7f3e9f756db61266996a1a07ba146196a3e93e
Sreenidhi <sreenidhibs04@gmail.com>
2026-03-15 21:17:28 +0530

    Add default values for profilePicture and bio in User model

--- Server/models/User.js
+++ Server/models/User.js
@@ -18,9 +18,11 @@ const userSchema = new Schema({
   },
   profilePicture: {
     type: String,
+    default: 'https://www.gravatar.com/avatar/00000000000000000000000000000000?d=mp&f=y',
   },
   bio: {
     type: String,
+    default: 'No bio available',
   },
   createdAt: {
     type: Date,
